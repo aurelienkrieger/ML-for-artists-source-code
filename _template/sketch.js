24fdf6200dc8a6ceca62d84017ab8aa41dcc5e09
Aurelien Krieger <a.krieger.b@gmail.com>
2023-12-04 07:24:21 +0000

    add boilerplate

---
+++ _template/sketch.js
@@ -0,0 +1,19 @@
+function preload() {
+
+}
+
+function setup() {
+  createCanvas(640, 480);
+}
+
+function draw() {
+  background(0);
+}
+
+function gotResult(error, results) {
+  if (error) {
+    console.error(error);
+    return;
+  }
+
+}
